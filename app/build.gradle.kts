67dc1ae3a6768b5e542d52a08c11dbf64dddfa6d
goofwear <goofwear@gmail.com>
2025-10-11 23:55:55 -0400

    Bump versionCode to 4 and versionName to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.goofwear.file85toolkit"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.0.1"
     }
 
     buildTypes {
